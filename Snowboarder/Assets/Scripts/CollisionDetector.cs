6644c31bcef0f5d2521b8f6d3b8474f7d38bfdc0
AdamKretowicz <adam.Kretowicz99@gmail.com>
2022-07-06 18:09:22 +0200

    Adjusted the finish/crash effects

--- Snowboarder/Assets/Scripts/CollisionDetector.cs
+++ Snowboarder/Assets/Scripts/CollisionDetector.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class CollisionDetector : MonoBehaviour
 {
     [SerializeField] float onCrashDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
 
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", onCrashDelay);
         }
     }    
