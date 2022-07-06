5b065238578d477a901791ccebd73897fdae2cea
AdamKretowicz <adam.Kretowicz99@gmail.com>
2022-07-06 12:25:22 +0200

    Added a delay between reloading scenes

--- Snowboarder/Assets/Scripts/CollisionDetector.cs
+++ Snowboarder/Assets/Scripts/CollisionDetector.cs
@@ -5,11 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class CollisionDetector : MonoBehaviour
 {
+    [SerializeField] float onCrashDelay = 0.5f;
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", onCrashDelay);
         }
+    }    
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
+    
 }
